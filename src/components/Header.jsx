08ae28ecf47d6968b136f1630c611fcdf641df1d
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-03-30 17:22:15 +0000

    complete second heaer link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 function Header() {
   return (
-    <nav
-    class="relative flex w-full flex-nowrap items-center justify-between bg-theme-color-655dbb py-4 lg:flex-wrap lg:justify-start"
-    data-te-navbar-ref>
-    <div class="flex w-full flex-wrap items-center justify-between px-6">
-        <div class="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto">
-            <a class="text-4xl text-white theme-font-lato font-bold" href="#">TradR</a>
+    <nav class="relative flex w-full flex-nowrap items-center justify-between bg-theme-color-655dbb theme-font-lato py-4 lg:flex-wrap lg:justify-start" data-te-navbar-ref>
+        <div class="flex w-full flex-wrap items-center justify-between px-6">
+            {/* Grows the left div till 100%, pushing the next to the left */}
+            <div class="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto">
+                <a class="text-4xl text-white font-bold" href="#">TradR</a>
+            </div>
+            <div class="relative flex items-center">
+                <a class="text-3xl text-white font-normal" href="#">login</a>
+            </div>
         </div>
-    </div>
     </nav>
   )
 }
